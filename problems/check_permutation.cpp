1c207d7aadbf449e2c4888cc39caaed118db9575
Samuel Bautista <samuelbaubas@gmail.com>
2022-09-23 19:25:42 -0500

    problem 1.2 CtCI

--- problems/check_permutation.cpp
+++ problems/check_permutation.cpp
@@ -1,5 +1,48 @@
+#include <iostream>
+#include <map>
+#include <string>
 
-int main() {
-    
+bool checkPermutation(std::string first, std::string second)
+{
+    if (first.length() != second.length())
+    {
+        return false;
+    }
+    std::map<char, int> storage;
+    std::pair<char, int> aux;
+    for (int i = 0; i < first.length(); i++)
+    {
+        char position = first.at(i);
+        storage[position]++;
+    }
+
+    for (auto j = 0; j < second.length(); j++)
+    {
+        char position = second.at(j);
+        if (storage.find(position) == storage.end())
+        {
+            return false;
+        }
+        storage[position]--;
+        if (storage[position] < 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    std::string str1 = "triangle", str2 = "integral";
+    if (checkPermutation(str1, str2))
+    {
+        std::cout << "2 es permutacion de 1" << std::endl;
+    }
+    else
+    {
+        std::cout << "2 no es permutacion de 1" << std::endl;
+    }
     return 0;
 }
