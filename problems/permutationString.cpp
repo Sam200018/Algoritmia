8d1849333f42120ef6b8a3cc764060b33d34bcd9
Samuel Bautista <samuelbaubas@gmail.com>
2022-10-03 18:13:24 -0500

    Time Limit Exceeded: we passed every test case but it takes too long, sorry I didn't finish my code

--- problems/permutationString.cpp
+++ problems/permutationString.cpp
@@ -5,52 +5,70 @@
 class permutationString
 {
 public:
-        bool checkInclusion(std::string s1, std::string s2){
-            if(s1.length()>s2.length()) return false;
-            std::unordered_map<char,int> s1Map;
-            std::unordered_map<char,int> s2Map;
-            for(char ch:s1){
-                if(s1Map.find(ch)==s1Map.end()){
-                    s1Map[ch]=1;
-                }else{
-                    s1Map[ch]++;
-                }
+    bool checkInclusion(string s1, string s2)
+    {
+        if (s1.length() > s2.length())
+            return false;
+        std::unordered_map<char, int> s1Map;
+        std::unordered_map<char, int> s2Map;
+        for (char ch : s1)
+        {
+            if (s1Map.find(ch) == s1Map.end())
+            {
+                s1Map[ch] = 1;
             }
-
-            for(int i=0; i<=s2.length()-s1.length();i++){
-                for(int j=0;j<s1.length();j++){
-                    char aux=s2[i+j];
-                    if(s2Map.find(aux)==s2Map.end()){
-                        s2Map[aux]=1;
-                    }else{
-                        s2Map[aux]++;
-                    }
-                }
-                if(mapMatches(s1Map,s2Map,s1))
-
-                s2Map.clear();
+            else
+            {
+                s1Map[ch]++;
             }
         }
 
-        bool mapMatches(std::unordered_map<char,int> map1,std::unordered_map<char,int> map2,std::string s1){
-            for(char key: s1){
-                    if(map2.find(key)==map2.end()){
-                        if(map1.at(key)-(-1)!=0){
-                            return false;
-                        }
-                    }else{
-                        if(map1.at(key)-map2.at(key)!=0){
-                            return false;
-                        }
-                    }
+        for (int i = 0; i <= s2.length() - s1.length(); i++)
+        {
+            for (int j = 0; j < s1.length(); j++)
+            {
+                char aux = s2[i + j];
+                if (s2Map.find(aux) == s2Map.end())
+                {
+                    s2Map[aux] = 1;
+                }
+                else
+                {
+                    s2Map[aux]++;
+                }
             }
-            return true;
+            if (mapMatches(s1Map, s2Map, s1))
+            {
+                return true;
+            }
+
+            s2Map.clear();
         }
+        return false;
+    }
+
+    bool mapMatches(std::unordered_map<char, int> map1, std::unordered_map<char, int> map2, std::string s1)
+    {
+        for (char key : s1)
+        {
+            if (map2.find(key) == map2.end())
+            {
+                if (map1.at(key) - (-1) != 0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (map1.at(key) - map2.at(key) != 0)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 };
-
-
-
-
 
 int main(int argc, char const *argv[])
 {
